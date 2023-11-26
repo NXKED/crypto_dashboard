b7e119ea9a0f686e661d7e63421f9acdebf11573
Nyx <nxked@proton.me>
2023-11-26 19:20:55 +0100

    Update publicPath for images

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             options: {
               name: "[name].[ext]",
               outputPath: "images/",
-              publicPath: "images/",
+              publicPath: "./dist/images/",
             },
           },
         ],
