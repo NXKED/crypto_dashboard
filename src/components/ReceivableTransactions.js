79b3a76b74041f9174ac05ffde9218a608efa402
NXKED <nxked@proton.me>
2024-01-08 16:10:39 +0100

    Add BPbalance and PWbalance states to ReceivableTransactions component

--- src/components/ReceivableTransactions.js
+++ src/components/ReceivableTransactions.js
@@ -5,6 +5,8 @@ import { BANWALLET } from "../const";
 const ReceivableTransactions = () => {
   const [transactions, setTransactions] = useState(null);
   const [totalAmount, setTotalAmount] = useState(null);
+  const [BPbalance, setBPbalance] = useState(null);
+  const [PWbalance, setPWbalance] = useState(null);
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -16,11 +18,21 @@ const ReceivableTransactions = () => {
           }
         );
 
+        const BPOverview = await axios.get(
+          "https://api.spyglass.pw/banano/v1/account/overview/ban_1boompow14irck1yauquqypt7afqrh8b6bbu5r93pc6hgbqs7z6o99frcuym",
+        );
+
+        const PWOverview = await axios.get(
+          "https://api.spyglass.pw/banano/v1/account/overview/ban_1igof5isd3xxn7yen8owx1m9cje68mt3n3cpyp8kqkzkcr5x6hwcxwy1pzmq",
+        );
+
         if (!response.data) {
           console.error("No data in the response.");
           return;
         }
 
+        const BPbalance = BPOverview.data;
+        const PWbalance = PWOverview.data;
         const transactionData = response.data;
         const sortedTransactions = response.data
           .slice()
@@ -29,8 +41,10 @@ const ReceivableTransactions = () => {
           (acc, transaction) => acc + transaction.amount,
           0
         );
+        
         setTotalAmount(sumBanReceived);
-
+        setBPbalance(BPbalance);
+        setPWbalance(PWbalance);
         setTransactions(sortedTransactions);
       } catch (error) {
         console.error("Error fetching transactions:", error);
@@ -66,8 +80,9 @@ const ReceivableTransactions = () => {
     <div className="receivable-transactions">
       {transactions ? (
         <div>
-          <h2>Receivable Transactions</h2>
+          <h2>Receivable Transactions </h2>
           <p>Total Pending Amount: {totalAmount.toFixed(0)} BAN</p>
+          <p>Boom-Balance: {BPbalance.balance}</p>
           <ul>
             {transactions.map((transaction) => (
               <li key={transaction.hash} className="list-items">
@@ -98,6 +113,7 @@ const ReceivableTransactions = () => {
               </li>
             ))}
           </ul>
+          <p>Balance: {PWbalance.balance.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</p>
         </div>
       ) : (
         <p>Loading receivable transactions...</p>
